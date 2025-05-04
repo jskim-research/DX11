09ca47c569d60f7bf5af083446d8aacc61e93ec5
jskim-research <wltnwkd21@gmail.com>
2025-05-04 17:22:40 +0900

    Chore: upload initial framework file

---
+++ DX11/applicationclass.cpp
@@ -0,0 +1,32 @@
+#include "applicationclass.h"
+
+ApplicationClass::ApplicationClass()
+{
+}
+
+ApplicationClass::ApplicationClass(const ApplicationClass&)
+{
+}
+
+ApplicationClass::~ApplicationClass()
+{
+}
+
+bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	return true;
+}
+
+void ApplicationClass::Shutdown()
+{
+}
+
+bool ApplicationClass::Frame()
+{
+	return true;
+}
+
+bool ApplicationClass::Render()
+{
+	return true;
+}
---
+++ DX11/applicationclass.h
@@ -0,0 +1,38 @@
+/*
+그래픽 관련 클래스
+*/
+
+#pragma once
+#ifndef _APPLICATIONCLASS_H_
+#define _APPLICATIONCLASS_H_
+
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.3f;
+
+
+#endif
+class ApplicationClass
+{
+public:
+	ApplicationClass();
+	ApplicationClass(const ApplicationClass&);
+	~ApplicationClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+};
